85c227cdf14ef07c6fd9d05e6cd9277c073f8c75
Kacper Kozimor <kkozimor@objectivity.co.uk>
2020-03-19 10:20:31 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## NavigationItemSelector
+Mendix plugable widget that marks navigation item as active based on menu item name and navigation item name provided by user.
+
+## About
+This widget was created to help handling with navigating to a page in other way that through navigation menu e.g. button, deep link etc.
+
+## Usage
+Place the widget en every page that can be accessed through navigation of your app.
+Insert values:
+- Menu widget name - name of menu component placed on your layout e.g. "navigationTree3", "menuBar1"
+- Menu item name - caption of menu item that sould be activated
---
+++ src/NavigationItemSelector.jsx
@@ -0,0 +1,54 @@
+import { Component, createElement } from "react";
+import { hot } from "react-hot-loader/root";
+
+class NavigationItemSelector extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    findItem = (menu, itemName) => {
+        if (menu == null) return null;
+        var items = menu.getElementsByTagName("li");
+        var item;
+        Array.from(items).forEach(it => {
+            if (it.innerText === itemName) {
+                item = it;
+                return;
+            }
+        });
+        return item.getElementsByTagName("a")[0];
+    };
+
+    activateItem = (menu, item) => {
+        Array.from(menu.getElementsByTagName("li")).forEach(element => {
+            var a = element.getElementsByTagName("a")[0];
+            a.classList.remove("active");
+        });
+        item.classList.add("active");
+    };
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            if (this.props.navigationItem.status == "available" && this.props.menuName.status == "available") {
+                var menu = document.querySelector(".mx-name-" + this.props.menuName.value);
+                var item = this.findItem(menu, this.props.navigationItem.value);
+                if (item != null) {
+                    if (item.classList.contains("active") == true) {
+                        clearInterval(this.interval);
+                    } else {
+                        console.log(item);
+                        this.activateItem(menu, item);
+                        item.focus();
+                        console.log(item);
+                    }
+                }
+            }
+        }, 500);
+    }
+
+    render() {
+        return <div style={{ display: "none" }}></div>;
+    }
+}
+
+export default hot(NavigationItemSelector);
