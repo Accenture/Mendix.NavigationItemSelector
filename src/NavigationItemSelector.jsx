aea03d86e026d896533bd86113e44dc7b172fcf9
kkozimor <kkozimor@objectivity.co.uk>
2021-09-22 12:51:42 +0200

    added navigation list widget support, increased version number, added select by index option

--- src/NavigationItemSelector.jsx
+++ src/NavigationItemSelector.jsx
@@ -12,14 +12,40 @@ class NavigationItemSelector extends Component {
         return widget;
     }
 
-    findItem(widget, caption) {
-        const items = widget._menuItemMap;
+    findItem(widget, caption, index) {
         let item;
-        Object.keys(items).forEach(key => {
-            if (items[key].caption === caption) {
-                item = items[key].id;
+        let items;
+        if (this.props.menuType === "navlist") {
+            if (caption != null) {
+                items = widget.getChildren();
+                for (let i = 0; i < items.length; i++) {
+                    if (items[i].domNode.innerText === caption) {
+                        item = i;
+                    }
+                }
+            } else if (index != null) {
+                item = index;
+            } else {
+                console.error("Navigation item selector configuration error!");
             }
-        });
+        } else {
+            items = widget._menuItemMap;
+            let i = 0;
+            Object.keys(items).forEach(key => {
+                if (caption != null) {
+                    if (items[key].caption === caption) {
+                        item = items[key].id;
+                    }
+                } else if (index != null) {
+                    if (i === this.props.itemIndex) {
+                        item = items[key].id;
+                    }
+                    i++;
+                } else {
+                    console.error("Navigation item selector configuration error!");
+                }
+            });
+        }
         return item;
     }
 
@@ -28,7 +54,7 @@ class NavigationItemSelector extends Component {
         widget._currentSelection = itemId;
         widget._initialActive = widget._currentActive;
         widget._initialSelection = widget._currentSelection;
-        if (widget._currentActive && this.props.menuType === "navtree") {
+        if ((widget._currentActive && this.props.menuType === "navtree") || this.props.menuType === "navlist") {
             widget._view.deactivateAll();
             widget._view.activate(widget._currentActive);
         } else if (widget._currentActive && this.props.menuType === "menubar") {
@@ -39,12 +65,28 @@ class NavigationItemSelector extends Component {
 
     componentDidMount() {
         this.interval = setInterval(() => {
-            if (this.props.menuName.status === "available" && this.props.itemCaption.status === "available") {
+            if (
+                this.props.menuName.status === "available" &&
+                (this.props.selectorType === "byIndex" || this.props.itemCaption.status === "available")
+            ) {
                 const widget = this.getWidget(this.props.menuName.value);
                 if (widget && widget._loaded) {
-                    const itemId = this.findItem(widget, this.props.itemCaption.value);
-                    this.activateItem(widget, itemId);
-                    clearInterval(this.interval);
+                    let itemId;
+                    try {
+                        itemId =
+                            this.props.selectorType === "byIndex"
+                                ? this.findItem(widget, null, this.props.itemIndex)
+                                : this.findItem(widget, this.props.itemCaption.value, null);
+                        if (itemId !== undefined) {
+                            this.activateItem(widget, itemId);
+                        } else {
+                            console.error("Navigation item selector configuration error!");
+                        }
+                        clearInterval(this.interval);
+                    } catch (e) {
+                        console.error("Navigation item selector configuration error!");
+                        clearInterval(this.interval);
+                    }
                 }
             }
         }, 50);
